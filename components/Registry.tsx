ea15a0f26fdd5e12d8e8379300cde57f6972734f
Patrick O'Leary <poleary@mitre.org>
2023-10-26 20:44:55 -0400

    registry url

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -12,8 +12,10 @@ export default function Registry() {
         <div className="flex flex-col items-center space-y-12 px-4">
             <div className="flex-1 flex flex-col space-y-12 text-center">
                 <div className="text-5xl">Wedding Registry</div>
-                <div className="flex flex-col items-center space-y-5 text-2xl">
-                  <div>Registry will become available soon</div>
+                <div className="flex flex-col items-center space-y-5">
+                    <a  href="https://www.zola.com/registry/patrickandsophia2024" target="_blank">
+                      <Button style={{ color: 'black', backgroundColor: 'white', width: '230px' }} variant="contained">Registry</Button>
+                    </a>
                 </div>
             </div>
         </div>
